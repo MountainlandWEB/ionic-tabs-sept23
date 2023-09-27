344da1a7dc248aefcb7a2d97c12c2d92da525ba9
Todd H <github@simplybest.com>
2023-09-27 08:30:28 -0600

    styling changes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { AboutPage } from '../about/about.page';
+import { Badge } from '@awesome-cordova-plugins/badge/ngx';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule, CommonModule],
+  providers: [Badge],
 })
-export class Tab1Page {
-  constructor(private modal: ModalController) {
+export class Tab1Page implements OnInit {
+  constructor(private modal: ModalController, private badge: Badge) {
 
   }
 
+  ngOnInit() {
+    this.setBadgeCount();
+  }
+
   group = "friends";
+  badgeCount = 3;
 
   async openAbout() {
     const modal1 = await this.modal.create({
@@ -22,5 +31,26 @@ export class Tab1Page {
     });
     
     modal1.present();
+  }
+
+  async setBadgeCount() {
+    try {
+      await this.badge.set(this.badgeCount);
+    } catch (e) {
+      console.log(e);
+    }
+    const evt = new CustomEvent('badgeCount', { detail: this.badgeCount });
+    window.dispatchEvent(evt);
+  }
+
+  async clearBadgeCount() {
+    this.badgeCount = 0;
+    try {
+      await this.badge.clear();
+    } catch (e) {
+      console.log(e);
+    }
+    const evt = new CustomEvent('badgeCount', { detail: this.badgeCount });
+    window.dispatchEvent(evt);
   }
 }
